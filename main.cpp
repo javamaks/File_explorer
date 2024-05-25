49c77510e8d28c09116bc23d598d310b07c7670b
Жава <63841611+javamaks@users.noreply.github.com>
2024-05-25 23:58:55 +0500

    small changes

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("FileSync - файловый менеджер");
+    w.setWindowIcon(QIcon(":/app.jpg"));
     w.show();
     return a.exec();
 }
---
+++ ui_mainwindow.h
@@ -0,0 +1,101 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.6.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QMenuBar *menubar;
+    QMenu *menu;
+    QMenu *menuEdit;
+    QMenu *menuView;
+    QMenu *menuSort;
+    QMenu *menuGo;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(10, 10, 91, 31));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+        pushButton_2->setGeometry(QRect(570, 310, 181, 81));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        menu = new QMenu(menubar);
+        menu->setObjectName("menu");
+        menuEdit = new QMenu(menubar);
+        menuEdit->setObjectName("menuEdit");
+        menuView = new QMenu(menubar);
+        menuView->setObjectName("menuView");
+        menuSort = new QMenu(menubar);
+        menuSort->setObjectName("menuSort");
+        menuGo = new QMenu(menubar);
+        menuGo->setObjectName("menuGo");
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menu->menuAction());
+        menubar->addAction(menuEdit->menuAction());
+        menubar->addAction(menuView->menuAction());
+        menubar->addAction(menuSort->menuAction());
+        menubar->addAction(menuGo->menuAction());
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "\320\236\321\202\320\272\321\200\321\213\321\202\321\214 \321\204\320\260\320\271\320\273", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "\320\235\321\203\321\200\320\261\320\265\320\272 \321\207\321\203\321\207\320\274\320\265\320\272", nullptr));
+        menu->setTitle(QCoreApplication::translate("MainWindow", "\320\244\320\260\320\271\320\273", nullptr));
+        menuEdit->setTitle(QCoreApplication::translate("MainWindow", "Edit", nullptr));
+        menuView->setTitle(QCoreApplication::translate("MainWindow", "View", nullptr));
+        menuSort->setTitle(QCoreApplication::translate("MainWindow", "Sort", nullptr));
+        menuGo->setTitle(QCoreApplication::translate("MainWindow", "Go", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
